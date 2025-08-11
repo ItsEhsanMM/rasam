fbc200566e325de280bd5816cacd943950764444
ItsEhsanMM <itsehsanmm@gmail.com>
2025-08-11 15:23:30 +0330

    feat: add desktop navbar

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,14 +1,12 @@
 import { Drawer, DrawerTrigger } from '@/components/ui/drawer';
 import SideBar from './Navbar/SideBar';
 import { Menu } from 'lucide-react';
+import Navbar from './Navbar/Navbar';
 
 const Header = () => {
   return (
     <div className="border-primary/30 flex h-20 flex-row-reverse items-center justify-between rounded-lg border px-5">
-      <div className="text-primary flex gap-x-2 text-xl font-bold">
-        <h1>LOGO</h1>
-        <h1>نوشته لوگو</h1>
-      </div>
+      <Navbar />
       <Drawer direction="right" modal={false}>
         <DrawerTrigger className="md:hidden">
           <Menu />
--- components/shared/Navbar/Navbar.tsx
+++ components/shared/Navbar/Navbar.tsx
@@ -1,4 +1,31 @@
+import { navlinks } from '@/lib/const';
+import { PhoneCall } from 'lucide-react';
+import Link from 'next/link';
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="hidden w-full items-center justify-between md:flex">
+      <div>LOGO</div>
+      <div className="flex w-1/2 justify-between">
+        {navlinks.map((link) => (
+          <Link
+            className="hover:text-primary underline-offset-10 transition-all hover:underline"
+            key={link.name}
+            href={link.href}
+          >
+            {link.name}
+          </Link>
+        ))}
+      </div>
+      <div>
+        <Link
+          href={'tel:+989370814221'}
+          className="bg-primary block cursor-pointer rounded-full p-1.5 text-white"
+        >
+          <PhoneCall strokeWidth={1} direction={'rtl'} />
+        </Link>
+      </div>
+    </nav>
+  );
 };
 export default Navbar;
